6ee9c16c4bb07f533580488db14833a50646ffa5
Дмитрий Пошехонов <toycru@ya.ru>
2023-04-24 00:16:21 +0300

    Добавлно приложение users и класс, расширяющий настройки пользователя

--- test_em_all/users/models.py
+++ test_em_all/users/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+class User(AbstractUser):
+    about_info = models.TextField(
+    'О себе',
+    blank=True,
+    )
 
-# Create your models here.
