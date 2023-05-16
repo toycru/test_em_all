8e20404f4d7a245f8f7585b7920e265209f459a7
Дмитрий Пошехонов <toycru@ya.ru>
2023-05-17 00:26:56 +0300

    Create models users and models surveys

--- test_em_all/surveys/models.py
+++ test_em_all/surveys/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Survey(models.Model):
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор'
+    )
+    subject = models.ForeignKey(
+        "Дисциплина",
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='surveys',
+        verbose_name='Дисциплина',
+        help_text='Выберите дисциплину'
+    )
+    title = models.TextField(
+        'Название теста',
+        help_text='Введите название теста'
+    )
+    question = models.ForeignKey(
+        "Question",
+    )
+    # criterion
+
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+    # title = models.CharField(max_length=200)
+    text = models.TextField(
+        'Текст вопроса',
+        help_text='Введите текст вопроса'
+    )
+    number = models.CharField(max_length=9)
+
+    def __str__(self):
+        return self.title
